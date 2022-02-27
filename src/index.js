ac4f567819cf073f4cacc050b530508cc32be412
henryburkhardt <henrywb04@gmail.com>
2022-02-27 17:43:36 -0600

    deleting index.css and logo.svg (undeaded)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <StrictMode>
